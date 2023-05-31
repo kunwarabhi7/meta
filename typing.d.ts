af9f09f0e295e4b72e88fe6f6fb5c654137744bb
Abhishek <abhishekparmar162@gmail.com>
2023-05-31 10:43:58 +0530

    added type config

---
+++ typing.d.ts
@@ -0,0 +1,11 @@
+export type Message = {
+    id:string,
+    mesaage:string,
+    created_at:number,
+    username:string,
+    profile_pic: string,
+    email:string,
+}
+
+
+export default Message;
